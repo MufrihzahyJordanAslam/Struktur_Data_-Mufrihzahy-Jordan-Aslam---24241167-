0d251ae4bdcdcac1f10f71c23778f34cd3a8fd01
Jordan <mufrihjordan@gmail.com>
2025-04-11 09:48:57 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,25 @@
+# perbandingan kemungkinan nilai = ture/salah (bolean
+# 1 > 2 =
+# <, >, >=, <=, !=, not
+# a = 24
+# b = 67
+
+a = 24
+b = 67
+hasil = a < b
+print(hasil)
+
+hasil = a > b
+print(hasil)
+
+hasil = a >= b
+print(hasil)
+
+hasil = a <= b
+print(hasil)
+
+
+
+hasil = a != b
+print(hasil)
+
